76e6722f973a9355eadc96e196d1bcb24b90139b
Caleb Reese <caleb22reese@gmail.com>
2024-03-15 21:19:21 -0600

    team names

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './CollegeBasketballTeams.json';
 
+interface TeamProps {
+  name: string;
+  mascot: string;
+  location: string;
+}
+const teamNames = [
+  {
+    name: 'BYU',
+    mascot: 'Cougar',
+    location: 'Provo, Utah',
+  },
+  {
+    name: 'UCLA',
+    mascot: 'Bruin',
+    location: 'Los Angeles',
+  },
+  {
+    name: 'Texas',
+    mascot: 'Longhorns',
+    location: 'Austin, Texas',
+  },
+  {
+    name: 'UVU',
+    mascot: 'Wolverine',
+    location: 'Orem, Utah',
+  },
+];
+
+function Title() {
+  return <h1>March Madness</h1>;
+}
+
+function Welcome() {
+  return (
+    <h3>
+      March Madness is upon us. In order to prepare, I made this website to give
+      you a little refresher on all the teams
+    </h3>
+  );
+}
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>{oneTeam.name}</h2>
+        <h3>Mascot: {oneTeam.mascot}</h3>
+        <h3>Location: {oneTeam.location}</h3>
+      </div>
+    );
+  }
+}
+
+function CollegeList() {
+  return (
+    <div>
+      {teamNames.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <Welcome />
+      <CollegeList />
     </div>
   );
 }
